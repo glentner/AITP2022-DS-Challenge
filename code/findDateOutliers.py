b03cdc4f8dc0cfc50100cd5f47f6fdd21df497be
Ai0796 <mo.alden25@gmail.com>
2022-04-02 13:24:43 -0400

    Creates a date outlier finder

---
+++ code/findDateOutliers.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import os
+import pandas as pd
+from pyod.models.ecod import ECOD
+
+def getDate(dateString):
+    # print(dateString)
+    strpDate = dateString.replace("-", "")
+    
+    return datetime.strptime(strpDate, "%Y%m%d")
+
+def isOutlier(date, lastDate, nextDate):
+    
+    date = getDate(date)
+    lastDate = getDate(lastDate)
+    nextDate = getDate(nextDate)
+    
+    dateDiffBefore = lastDate - date
+    dateDiffAfter = nextDate - date
+    
+    ##If it's two dates from different years then it doesn't matter
+    if dateDiffBefore.days > 365 or dateDiffAfter.days > 365:
+        return False
+    
+    ##More than 7 days of isolation it's an outlier
+    elif dateDiffAfter.days > 7 or dateDiffBefore.days > 7:
+        return True
+    
+    else:
+        return False
+
+def main():
+    outliers = 0
+    input = "combined/"
+    stations = {}
+    directories = os.listdir(input)
+    
+    for station in directories:
+        
+        path = os.path.join(input, station)
+        
+        df = pd.read_csv(path)
+        
+        dates = df["DATE"]
+        
+        oldDate = getDate(dates[0])
+        
+        for i in range(1, len(dates) - 1):
+            if(isOutlier(dates[i], dates[i - 1], dates[i + 1])):
+                print(dates[i], dates[i - 1], dates[i + 1])
+                outliers += 1
+                df.drop(i)
+                
+        df.to_csv(path)
+            # oldDate = getDate(date)'
+            
+    print(outliers)
+            
+    return 0
+        
+if __name__ == "__main__":
+    main()
